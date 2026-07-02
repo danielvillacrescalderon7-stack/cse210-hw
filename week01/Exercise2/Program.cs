dfef76c637e119b9fca44ac70c9b7748b98412fc
Daniel Villacres <danielvillacrescalderon7@gmail.com>
2026-07-02 12:39:21 -0500

    Excercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,88 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        //Ask the grade of the user
+        Console.Write ("What is your grade percentage?");
+        string userInput = Console.ReadLine(); 
+
+        int grade = int.Parse(userInput);
+
+        //Variable for the letter
+        string letter = "";
+
+        //Determine the letter of the grade 
+
+        if(grade >= 90)
+        {
+
+            letter = "A";
+
+        }
+        else if (grade >= 80)
+        {
+            
+            letter = "B";
+
+        }
+
+        else if (grade >= 70)
+        {
+                           
+            letter = "C"
+;
+        }
+
+        else if (grade >= 60)
+        {
+            
+            letter = "D";
+
+        }
+
+        else {letter = "F";}
+
+        //Determine if the course was approved
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You pass the course");
+        }
+        
+        else
+        {
+            Console.WriteLine( "You did not pass. Keep trying!");
+        }
+
+        //Calculate the + or -
+
+        int lastDigit = grade % 10;
+        string sign = "";
+
+        if (grade >= 60 && grade <100)
+        {
+            if (lastDigit>= 7)
+            {
+                sign ="+";
+            }
+            else if (lastDigit >= 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (letter =="A" && sign == "+")
+        {
+            sign = "-"; 
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine("Your grade is:" + letter + sign);
+
     }
 }
