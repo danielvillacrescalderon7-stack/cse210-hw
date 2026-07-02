04a72496c8be8e33f6998fdc9933a72668d0da5f
Daniel Villacres <danielvillacrescalderon7@gmail.com>
2026-07-02 13:33:11 -0500

    Excercise 4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+       //saving numbers list
+     List<int> numbers = new List<int>();
+     Console.WriteLine ("Enter a list of numbers, type 0 when finished");
+     int number = -1;
+     
+     while (number != 0)
+        {
+            Console.Write("Enter Number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+int sum = 0;
+foreach(int num in numbers)
+        {
+            sum = sum + num;
+        
+        }
+        //average
+
+        double average = (double)sum / numbers.Count;
+
+        int max = numbers[0];
+        foreach(int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+Console.WriteLine($"The sum is: {sum}");
+Console.WriteLine($"The average is: {average}");
+Console.WriteLine($"The largest number is: {max}");
+
+int smallestPossitive = int.MaxValue;
+foreach(int num in numbers)
+        {
+            if (num > 0 && num < smallestPossitive)
+            {
+                smallestPossitive = num;
+            }
+        }
+if (smallestPossitive != int.MaxValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPossitive}");
+        }
+
+numbers.Sort();
+Console.WriteLine("Thesorted list is: ");
+foreach(int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
