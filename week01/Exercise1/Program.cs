888806495425c2099be6b17d911d393408f836bc
Daniel Villacres <danielvillacrescalderon7@gmail.com>
2026-07-02 10:38:25 -0500

    Added a note

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Ask the userfor their name.
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
