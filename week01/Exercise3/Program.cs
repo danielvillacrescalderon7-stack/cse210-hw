350f63f9bf6c0c84eac74e465ba7a1de2111ed30
Daniel Villacres <danielvillacrescalderon7@gmail.com>
2026-07-02 13:00:39 -0500

    Excercise 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random ();
+            int magicNumber = randomGenerator.Next (1, 101);
+
+            int guess = 0;
+            int guesses = 0;
+            while(guess != magicNumber)
+            {
+                Console.Write("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+                guesses++;
+
+                if (guess<magicNumber)
+                {
+                   Console.WriteLine ("Higher") ;
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guess it!");
+                }
+            }
+            Console.WriteLine("Number of guesses" + guesses);
+            Console.WriteLine("Do you want to continue?");
+            playAgain = Console.ReadLine();
+        }
     }
 }
