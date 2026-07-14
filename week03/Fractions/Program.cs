1ffe12f4ec6b782c293d300d4b778b0b8e00fc2c
Daniel Villacres <danielvillacrescalderon7@gmail.com>
2026-07-14 00:08:20 -0500

    learning Activity complete

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine();
+
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine();
+
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine();
+
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        Console.WriteLine();
+
+
+        Fraction f5 = new Fraction();
+
+        f5.SetTop(6);
+        f5.SetBottom(7);
+
+        Console.WriteLine("Testing getters and setters:");
+        Console.WriteLine($"Top: {f5.GetTop()}");
+        Console.WriteLine($"Bottom: {f5.GetBottom()}");
+        Console.WriteLine($"Fraction: {f5.GetFractionString()}");
+        Console.WriteLine($"Decimal: {f5.GetDecimalValue()}");
     }
 }
